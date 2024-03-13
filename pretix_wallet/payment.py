fac3762e625b8dfee8acd1fac86a898d201733af
Ben Bals <benbals@posteo.eu>
2024-03-13 18:05:20 +0100

    write proof of concept for wallet top-ups

--- pretix_wallet/payment.py
+++ pretix_wallet/payment.py
@@ -1,10 +1,11 @@
+import ast
 from _decimal import Decimal
 from collections import OrderedDict
 from typing import Dict, Any, Union
 
 from django.contrib import messages
 from django.db import transaction
-from django.forms import CharField
+from django.forms import CharField, MultipleChoiceField, CheckboxSelectMultiple
 from django.http import HttpRequest
 from django.utils.crypto import get_random_string
 from django.utils.translation import gettext_lazy as _
@@ -13,6 +14,7 @@ from pretix.base.models import Order, OrderPayment, GiftCard
 from pretix.base.models.customers import CustomerSSOProvider
 from pretix.base.payment import PaymentException, GiftCardPayment
 from pretix.base.services.cart import add_payment_to_cart
+from pretix.control.forms.item import ItemVariationForm
 from pretix.helpers import OF_SELF
 from pretix.multidomain.urlreverse import build_absolute_uri
 from pretix.presale.views.cart import cart_session
@@ -25,10 +27,10 @@ class WalletPaymentProvider(GiftCardPayment):
     verbose_name = _("Wallet")
     public_name = _("Wallet")
 
-    def payment_form_render(self, request: HttpRequest, total: Decimal, order: Order=None) -> str:
+    def payment_form_render(self, request: HttpRequest, total: Decimal, order: Order = None) -> str:
         return "Wallet payment form"
 
-    def checkout_confirm_render(self, request, order: Order=None, info_data: dict=None) -> str:
+    def checkout_confirm_render(self, request, order: Order = None, info_data: dict = None) -> str:
         return "Wallet confirm"
 
     def checkout_prepare(self, request: HttpRequest, cart: Dict[str, Any]) -> Union[bool, str]:
@@ -37,7 +39,8 @@ class WalletPaymentProvider(GiftCardPayment):
                 messages.error(request, _("You do not have a wallet."))
                 return False
             if request.customer.wallet.giftcard.value < 0:
-                messages.error(request, _("Your wallet has a negative balance. Please top it up or use another payment method."))
+                messages.error(request,
+                               _("Your wallet has a negative balance. Please top it up or use another payment method."))
                 return False
             cart_session(request)
             add_payment_to_cart(
@@ -47,7 +50,8 @@ class WalletPaymentProvider(GiftCardPayment):
                 info_data=self._get_payment_info_data(request.customer.wallet),
             )
             return True
-        return self._redirect_user(request, build_absolute_uri(request.event, "presale:event.checkout", kwargs={"step": "payment"}))
+        return self._redirect_user(request, build_absolute_uri(request.event, "presale:event.checkout",
+                                                               kwargs={"step": "payment"}))
 
     def _redirect_user(self, request: HttpRequest, next_url: str):
         provider = CustomerSSOProvider.objects.last()
@@ -73,6 +77,9 @@ class WalletPaymentProvider(GiftCardPayment):
             'retry': True
         }
 
+
+
+
     def execute_payment(self, request: HttpRequest, payment: OrderPayment, is_early_special_case=False) -> str:
         # re-implemented as the original method does not allow for giftcards to have negative balance
 
@@ -80,24 +87,15 @@ class WalletPaymentProvider(GiftCardPayment):
         if not gcpk:
             raise PaymentException("Invalid state, should never occur.")
         try:
-            with transaction.atomic():
-                try:
-                    gc = GiftCard.objects.select_for_update(of=OF_SELF).get(pk=gcpk)
-                except GiftCard.DoesNotExist:
-                    raise PaymentException(_("This gift card does not support this currency."))
-                if gc.currency != self.event.currency:  # noqa - just a safeguard
-                    raise PaymentException(_("This gift card does not support this currency."))
-                if not gc.accepted_by(self.event.organizer):
-                    raise PaymentException(_("This gift card is not accepted by this event organizer."))
+            try:
+                gc = GiftCard.objects.select_for_update(of=OF_SELF).get(pk=gcpk)
+            except GiftCard.DoesNotExist:
+                raise PaymentException(_("This gift card does not support this currency."))
 
-                trans = gc.transactions.create(
-                    value=-1 * payment.amount,
-                    order=payment.order,
-                    payment=payment,
-                    acceptor=self.event.organizer,
-                )
-                payment.info_data['transaction_id'] = trans.pk
-                payment.confirm(send_mail=not is_early_special_case, generate_invoice=not is_early_special_case)
+            trans = _wallet_transaction(self.event, payment, gc)
+            payment.info_data['transaction_id'] = trans.pk
+            payment.confirm(send_mail=not is_early_special_case, generate_invoice=not is_early_special_case)
+
         except PaymentException as e:
             payment.fail(info={'error': str(e)})
             raise e
@@ -108,7 +106,8 @@ class WalletPaymentProvider(GiftCardPayment):
                 messages.error(request, _("You do not have a wallet."))
                 return False
             if request.customer.wallet.giftcard.value < 0:
-                messages.error(request, _("Your wallet has a negative balance. Please top it up or use another payment method."))
+                messages.error(request,
+                               _("Your wallet has a negative balance. Please top it up or use another payment method."))
                 return False
             gc = request.customer.wallet.giftcard
             if gc not in self.event.organizer.accepted_gift_cards:
@@ -121,6 +120,41 @@ class WalletPaymentProvider(GiftCardPayment):
 
     @property
     def settings_form_fields(self):
+        product_choices = list(map(lambda product: (str(product.pk), product.name), self.event.items.all()))
+
         return OrderedDict(list(super().settings_form_fields.items()) + [
-            ('api_key', CharField(label=_("API key"), help_text=_("The API key that the terminal uses to authenticate against the POS api provided by this plugin."))),
+            ('api_key', CharField(label=_("API key"), help_text=_(
+                "The API key that the terminal uses to authenticate against the POS api provided by this plugin."))),
+            ('top_up_products', CharField(
+                label=_("Products used to charge wallet accounts"),
+                help_text=_(
+                  "Comma separated list of English product names, Buying these products will charge a users wallet by their price. Remeber, to require the Wallet memberships for these products to force users to login."),
+            ))
+            # ('top_up_products', MultipleChoiceField(
+            #     widget=CheckboxSelectMultiple,
+            #     label=_("Products used to charge wallet accounts"),
+            #     required=False,
+            #     initial=[0],
+            #     # initial=ast.literal_eval(self.event.settings.get('payment_wallet_top_up_products')),
+            #     help_text=_(
+            #         "Buying these products will charge a users wallet by their price. Remeber, to require the Wallet memberships for these products to force users to login."),
+            #     choices=product_choices))
         ])
+
+def _wallet_transaction(event, payment: OrderPayment, gift_card: GiftCard, sign=-1):
+    with transaction.atomic():
+        if gift_card.currency != event.currency:  # noqa - just a safeguard
+            raise PaymentException(_("This gift card does not support this currency."))
+        if not gift_card.accepted_by(event.organizer):
+            raise PaymentException(_("This gift card is not accepted by this event organizer."))
+
+        return gift_card.transactions.create(
+            value=sign * payment.amount,
+            order=payment.order,
+            payment=payment,
+            acceptor=event.organizer,
+        )
+def position_is_top_up_product(event, position):
+    top_up_products = event.settings.get("payment_wallet_top_up_products").lower().split(',')
+    product_name = position.item.name.localize('en').lower()
+    return product_name in top_up_products
--- pretix_wallet/signals.py
+++ pretix_wallet/signals.py
@@ -1,9 +1,18 @@
 from django.dispatch import receiver
-from pretix.base.signals import register_payment_providers
 
-from pretix_wallet.payment import WalletPaymentProvider
+from pretix.base.payment import PaymentException
+from pretix.base.signals import register_payment_providers, order_paid
+
+from pretix_wallet.payment import WalletPaymentProvider, _wallet_transaction
 
 
 @receiver(register_payment_providers, dispatch_uid="payment_wallet")
 def register_payment_provider(sender, **kwargs):
     return [WalletPaymentProvider]
+
+@receiver(order_paid, dispatch_uid="payment_wallet_order_paid")
+def wallet_order_paid(sender, order, **kwargs):
+    try:
+        _wallet_transaction(sender, order.payments.last(), order.customer.wallet.giftcard, sign=1)
+    except PaymentException as e:
+        raise e
