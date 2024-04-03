3d576fac16291fb956580f95d372b40aa1144b9b
Julian B <jeriox@users.noreply.github.com>
2024-04-03 16:49:31 +0200

    fix multidomain setup

---
+++ pretix_wallet/templatetags/wallet_tags.py
@@ -0,0 +1,8 @@
+from django import template
+from pretix.multidomain.urlreverse import build_absolute_uri
+
+register = template.Library()
+
+@register.simple_tag()
+def organizer_url(organizer, urlname):
+    return build_absolute_uri(organizer, urlname)
